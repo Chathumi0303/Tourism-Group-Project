aedf34bd7be77e9bd076864d68b1d08ea419b53d
Sadeepa Vitharanage <IT23761582@my.sliit.lk>
2025-05-21 13:02:15 +0530

    Update BaseModel.java

--- src/main/java/com/example/tourismpackage/models/BaseModel.java
+++ src/main/java/com/example/tourismpackage/models/BaseModel.java
@@ -1,1 +1,20 @@
+package com.example.tourismpackage.models;
 
+public class BaseModel {
+    private String id;
+
+    public BaseModel() {
+    }
+
+    public BaseModel(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
